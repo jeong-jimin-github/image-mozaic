a0bf400a245e6b00e9072903b3c12103951b3671
Jeong Jimin <k154085@kansai-u.ac.jp>
2026-08-24 21:48:27 +0900

    feat: activate modern UI on form load

---
+++ MainForm.ModernBootstrap.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ImageMosaicEditor;
+
+public partial class MainForm
+{
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        InitializeModernUi();
+    }
+}
